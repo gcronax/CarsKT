bd7806e70ddc229848218efab301fd9a81686ab8
guicamher <guillecampher15@gmail.com>
2025-09-11 20:36:39 +0200

    esfr

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,33 @@
-package org.example
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import kotlin.io.path.extension // Extensión de Kotlin para obtener la extensión
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val carpeta = Path.of("multimedia")
+    println("--- Iniciando la organización de la carpeta: " + carpeta + "---")
+    try {
+        Files.list(carpeta).use { streamDePaths ->
+            streamDePaths.forEach { pathFichero ->
+                if (Files.isRegularFile(pathFichero)) {
+                    val extension = pathFichero.extension.lowercase()
+                    if (extension.isBlank()) {
+                        println("-> Ignorando: " + pathFichero.fileName)
+                        return@forEach // Salta a la siguiente iteración del bucle
+                    }
+                    val carpetaDestino = carpeta.resolve(extension)
+                    if (Files.notExists(carpetaDestino)) {
+                        println("-> Creando nueva carpeta " + extension)
+                        Files.createDirectories(carpetaDestino)
+                    }
+                    val pathDestino = carpetaDestino.resolve(pathFichero.fileName)
+                    Files.move(pathFichero, pathDestino, StandardCopyOption.REPLACE_EXISTING)
+                    println("-> Moviendo " + pathFichero.fileName + " a " + extension)
+                }
+            }
+        }
+        println("\n--- ¡Organización completada con éxito! ---")
+    } catch (e: Exception) {
+        println("\n--- Ocurrió un error durante la organización ---")
+        e.printStackTrace()
     }
 }
