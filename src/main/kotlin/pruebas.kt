50fdea0680b329acab3df5136048e66472a64da5
guicamher <guillecampher15@gmail.com>
2025-09-18 20:55:45 +0200

    push n 11

--- src/main/kotlin/pruebas.kt
+++ src/main/kotlin/pruebas.kt
@@ -1,17 +1,16 @@
 import java.nio.file.Files
 import java.nio.file.Path
-import java.io.File
 // Clases de la librería oficial de Kotlin para la serialización/deserialización.
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 
 @Serializable
-data class Planta(val id_planta: Int, val nombre_comun: String, val
+data class Coche(val id_planta: Int, val nombre_comun: String, val
 nombre_cientifico: String, val frecuencia_riego: Int, val altura_maxima: Double)
 fun main() {
-    val entradaJSON = Path.of("datos_ini/mis_plantas.json")
-    val salidaJSON = Path.of("datos_ini/mis_plantas2.json")
-    val datos: List<Planta>
+    val entradaJSON = Path.of("datos_ini/coches.json")
+    val salidaJSON = Path.of("datos_ini/coches.json")
+    val datos: List<Coche>
     datos = leerDatosInicialesJSON(entradaJSON)
     for (dato in datos) {
         println(" - ID: ${dato.id_planta}, Nombre común: ${dato.nombre_comun}, " +
@@ -20,8 +19,8 @@ fun main() {
     }
     escribirDatosJSON(salidaJSON, datos)
 }
-fun leerDatosInicialesJSON(ruta: Path): List<Planta> {
-    var plantas: List<Planta> =emptyList()
+fun leerDatosInicialesJSON(ruta: Path): List<Coche> {
+    var coches: List<Coche> =emptyList()
     val jsonString = Files.readString(ruta)
     /* A `Json.decodeFromString` le pasamos el String con el JSON.
     Con `<List<Planta>>`, le indicamos que debe interpretarlo como
@@ -29,16 +28,16 @@ fun leerDatosInicialesJSON(ruta: Path): List<Planta> {
     La librería usará la anotación @Serializable de la clase Planta para saber
     cómo mapear los campos del JSON ("id_planta", "nombre_comun", etc.)
     a las propiedades del objeto. */
-    plantas = Json.decodeFromString<List<Planta>>(jsonString)
-    return plantas
+    coches = Json.decodeFromString<List<Coche>>(jsonString)
+    return coches
 }
-fun escribirDatosJSON(ruta: Path,plantas: List<Planta>) {
+fun escribirDatosJSON(ruta: Path, coches: List<Coche>) {
     try {
         /* La librería `kotlinx.serialization`
         toma la lista de objetos `Planta` (`List<Planta>`) y la convierte en una
         única cadena de texto con formato JSON.
         `prettyPrint` formatea el JSON para que sea legible. */
-        val json = Json { prettyPrint = true }.encodeToString(plantas)
+        val json = Json { prettyPrint = true }.encodeToString(coches)
 // Con `Files.writeString` escribimos el String JSON en el fichero de salida
         Files.writeString(ruta, json)
         println("\nInformación guardada en: $ruta")
